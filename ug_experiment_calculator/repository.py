b4b83935039e4bb6112e024deb4ad0306624ee2c
DarkPatrick <egorsiomin@yandex.ru>
2026-06-30 13:49:05 +0300

    removed date_end from hash

--- ug_experiment_calculator/repository.py
+++ ug_experiment_calculator/repository.py
@@ -808,7 +808,6 @@ def _experiment_users_hash_config(exp_info: dict, client: str, segment: dict) ->
         "client": client,
         "clients_options": clients_options,
         "date_start": exp_info.get("date_start", 0),
-        "date_end": exp_info.get("date_end", 0),
         "experiment_event_start": exp_info.get("experiment_event_start", ""),
     }
 
